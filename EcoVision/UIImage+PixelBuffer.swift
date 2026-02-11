6aaa4081f4519a630620c2041ee8eb5ac2e598a1
Abhishek <abhishek12439525@gmail.com>
2026-02-12 03:12:50 +0530

    Image helper

---
+++ EcoVision/UIImage+PixelBuffer.swift
@@ -0,0 +1,50 @@
+//
+//  UIImage+PixelBuffer.swift
+//  EcoVision
+//
+//  Created by Abhishek on 12/02/26.
+//
+
+import UIKit
+import CoreVideo
+
+extension UIImage {
+    func toPixelBuffer() -> CVPixelBuffer? {
+        let width = 299
+        let height = 299
+
+        var pixelBuffer: CVPixelBuffer?
+        let attrs = [
+            kCVPixelBufferCGImageCompatibilityKey: true,
+            kCVPixelBufferCGBitmapContextCompatibilityKey: true
+        ] as CFDictionary
+
+        CVPixelBufferCreate(
+            kCFAllocatorDefault,
+            width,
+            height,
+            kCVPixelFormatType_32ARGB,
+            attrs,
+            &pixelBuffer
+        )
+
+        guard let buffer = pixelBuffer else { return nil }
+
+        CVPixelBufferLockBaseAddress(buffer, [])
+        let context = CGContext(
+            data: CVPixelBufferGetBaseAddress(buffer),
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: CVPixelBufferGetBytesPerRow(buffer),
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue
+        )
+
+        guard let cgImage = self.cgImage else { return nil }
+        context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        CVPixelBufferUnlockBaseAddress(buffer, [])
+
+        return buffer
+    }
+}
