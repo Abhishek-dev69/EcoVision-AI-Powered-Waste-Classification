d61122328035330c17b3d513efc03d576c97a896
Abhishek <abhishek12439525@gmail.com>
2026-02-12 01:35:46 +0530

    Initial Commit

---
+++ EcoVision/EcoVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoVisionApp.swift
+//  EcoVision
+//
+//  Created by Abhishek on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcoVisionTests/EcoVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcoVisionTests.swift
+//  EcoVisionTests
+//
+//  Created by Abhishek on 12/02/26.
+//
+
+import Testing
+@testable import EcoVision
+
+struct EcoVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
