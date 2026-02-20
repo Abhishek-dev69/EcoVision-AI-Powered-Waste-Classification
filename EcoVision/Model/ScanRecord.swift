9f11979567b4f1539dc2df835a392406227812ec
Abhishek <abhishek12439525@gmail.com>
2026-02-20 11:12:19 +0530

    scan record feature added

--- EcoVision/Model/ScanRecord.swift
+++ EcoVision/Model/ScanRecord.swift
@@ -1,7 +1,25 @@
-//
-//  ScanRecords.swift
-//  EcoVision
-//
-//  Created by Abhishek on 19/02/26.
-//
+import Foundation
+
+struct ScanRecord: Identifiable, Codable {
+
+    let id: UUID
+    let label: String
+    let confidence: Double
+    let carbonFootprint: Double
+    let date: Date
+
+    init(
+        id: UUID = UUID(),
+        label: String,
+        confidence: Double,
+        carbonFootprint: Double,
+        date: Date
+    ) {
+        self.id = id
+        self.label = label
+        self.confidence = confidence
+        self.carbonFootprint = carbonFootprint
+        self.date = date
+    }
+}
 
