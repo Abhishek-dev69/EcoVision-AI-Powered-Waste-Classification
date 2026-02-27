bcda372ba74550591531e1bf91138fc084a822fb
Abhishek <abhishek12439525@gmail.com>
2026-02-27 23:55:32 +0530

    Manager file changes

--- EcoVision/Managers/ImpactEquivalenceManager.swift
+++ EcoVision/Managers/ImpactEquivalenceManager.swift
@@ -15,13 +15,11 @@ final class ImpactEquivalenceManager {
 
     func treesSaved(co2: Double) -> Double {
 
-        // 1 tree absorbs 21kg CO2/year
         return co2 / 21.0
     }
 
     func carKmEquivalent(co2: Double) -> Double {
 
-        // car emits 0.12kg/km
         return co2 / 0.12
     }
 }
